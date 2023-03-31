7190e611b813af6ce6e6032a77db2d8b2301f3c2
JP <85654561+jp524@users.noreply.github.com>
2023-03-31 19:59:42 -0400

    Add form input and onInputChange function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-function App() {
-  return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      task: { text: '' },
+    };
+  }
+
+  onInputChange = (e) => {
+    const input = e.target.value;
+    this.setState({
+      task: {
+        text: input,
+      },
+    });
+    console.log(this.state.task);
+  };
+
+  render() {
+    const { task } = this.state;
+
+    return (
+      <div>
+        <form>
+          <label htmlFor="taskInput">
+            Task
+            <input type="text" id="taskInput" onChange={this.onInputChange} />
+          </label>
+
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
